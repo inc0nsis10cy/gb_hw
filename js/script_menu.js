5f6173ef2193bbca4cbbc538f7267c0e96ef5aaf
met_ka <yekaterina.metelnikova@gmail.com>
2021-03-01 00:00:14 +0300

    hw-2

---
+++ js/script_menu.js
@@ -0,0 +1,35 @@
+'use strict'
+
+let brand_overlay = document.querySelector('.brand_overlay');
+let menu_icon = document.querySelector('.menu_icon');
+let cancel_menu = document.querySelector('.cancel_menu');
+let menu_wrap = document.querySelector('.menu_wrap');
+
+let filterParameters__size = document.querySelector('.filterParameters__size');
+let sizeCheckboxFilter = document.querySelector('.sizeCheckboxFilter');
+
+let filterLeft__menu = document.querySelector('.filterLeft__menu');
+let filterLeft = document.querySelector('.filterLeft');
+
+function toggleMenu() {
+    brand_overlay.classList.toggle('hidden');
+}
+function toggleMenu() {
+    menu_wrap.classList.toggle('hidden');
+}
+
+function toggleFilter() {
+    filterLeft__menu.classList.toggle('hidden');
+}
+
+function toggleCheckbox() {
+    sizeCheckboxFilter.classList.toggle('hidden');
+}
+
+cancel_menu.addEventListener('click', toggleMenu);
+menu_icon.addEventListener('click', toggleMenu);
+
+filterLeft.addEventListener('click', toggleFilter);
+
+filterParameters__size.addEventListener('click', toggleCheckbox);
+
