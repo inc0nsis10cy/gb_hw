cf288ebc34b5be28c7b2283686541b8f25ff2e2a
met_ka <yekaterina.metelnikova@gmail.com>
2021-03-10 16:57:34 +0300

    menu works, changes in js

--- js/script_menu.js
+++ js/script_menu.js
@@ -1,22 +1,15 @@
 'use strict'
-
-let brand_overlay = document.querySelector('.brand_overlay');
 let menu_icon = document.querySelector('.menu_icon');
 let cancel_menu = document.querySelector('.cancel_menu');
 let menu_wrap = document.querySelector('.menu_wrap');
 
 let filterParameters__size = document.querySelector('.filterParameters__size');
 let sizeCheckboxFilter = document.querySelector('.sizeCheckboxFilter');
-sizeCheckboxFilter.addEventListener('click', function (event) {
-    event.stopPropagation();
-})
+
 
 let filterLeft__menu = document.querySelector('.filterLeft__menu');
 let filterLeft = document.querySelector('.filterLeft');
 
-function toggleMenu() {
-    brand_overlay.classList.toggle('hidden');
-}
 function toggleMenu() {
     menu_wrap.classList.toggle('hidden');
 }
@@ -36,3 +29,6 @@ filterLeft.addEventListener('click', toggleFilter);
 
 filterParameters__size.addEventListener('click', toggleCheckbox);
 
+sizeCheckboxFilter.addEventListener('click', function (event) {
+    event.stopPropagation();
+})
